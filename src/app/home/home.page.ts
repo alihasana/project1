559d386e8365400b6fb893b9504a8b2f9374a8b5
alihasana <28J@n1988>
2019-04-08 15:26:50 +0200

    goLoginFb function in homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private auth: AuthService){}
+  theData: string;
+  goLoginFb() {
+    this.auth.facebookConnect().then((user) => {
+      console.log(user);
+      this.theData = JSON.stringify(user);
+    })
+  }
 
 }
