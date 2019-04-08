be67967fbdf9256beb76014dba05ea2814a7c296
alihasana <28J@n1988>
2019-04-08 16:49:49 +0200

    login setup in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { AuthService } from "./auth.service";
+
+import {environment} from "../environments/environment";
+
+// noinspection AngularInvalidImportedOrDeclaredSymbol,AngularInvalidImportedOrDeclaredSymbol
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+
   providers: [
+    Facebook,
+    AuthService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
